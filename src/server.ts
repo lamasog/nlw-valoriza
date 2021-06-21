b16aadcd1c0a5abf978c1ba076a12ac4e2508de7
Tungsinn <gvnalamas@tutanota.com>
2021-06-20 22:23:10 -0300

    Criação do servidor e rotas de teste

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+// @types/express
+const app = express();
+
+app.get("/test", (req, res) => {
+    return res.send("Olá, NLW.");
+});
+
+app.post("/test-post", (req, res) => {
+    return res.send("Olá, NLW método post.");
+})
+
+app.listen(3000, () => console.log('Server is running at port 3000'));
