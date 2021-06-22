eb446dac6d4be0ba5a861648b24a72670f066c0b
Tungsinn <gvnalamas@tutanota.com>
2021-06-22 14:10:19 -0300

    Criando rota /users

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 import express from "express";
 import "reflect-metadata";
-
+import { router } from "./routes";
 import "./database";
 
 // @types/express
 const app = express();
-
-app.get("/test", (req, res) => {
-    return res.send("Olá, NLW.");
-});
-
-app.post("/test-post", (req, res) => {
-    return res.send("Olá, NLW método post.");
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => console.log('Server is running at port 3000!'));
