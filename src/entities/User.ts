d04bf09528c9609024a5d78728838db6b5fe7ca9
Tungsinn <gvnalamas@tutanota.com>
2021-06-22 13:14:11 -0300

    Criação da entidade User

---
+++ src/entities/User.ts
@@ -0,0 +1,35 @@
+import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
+// a v4 gera números totalmente aleatórios
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+class User {
+
+    @PrimaryColumn()
+    // A alteração desse valor só é feita pela própria entidade com a biblioteca uuid
+    readonly id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn()
+    created_ad: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor() {
+        // Criar uuid para novos usuários
+        if(!this.id) {
+            this.id = uuid();
+        }
+    }
+}
+
+export { User };
