716a341d95441d8fb53169926ad6fb027e8233de
Tungsinn <gvnalamas@tutanota.com>
2021-06-22 13:46:28 -0300

    Criando service de usuários

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,37 @@
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IUserRequest {
+    name: string;
+    email: string;
+    // ? indica que o atributo é opcional
+    admin?: boolean;
+}
+
+class CreateUserService {
+    async execute({name, email, admin} : IUserRequest) {
+        const usersRepository = new UsersRepositories();
+
+        if(!email) {
+            throw new Error("Incorrect email");
+        }
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if(userAlreadyExists) {
+            throw new Error("User already exists");
+        }
+
+        const user = usersRepository.create({
+            name, 
+            email,
+            admin
+        })
+
+        await usersRepository.save(user);
+        return user;
+    }
+}
+
+export { CreateUserService };
