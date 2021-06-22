4557ebedce413caf9f94da28fac950a6e56ba171
Tungsinn <gvnalamas@tutanota.com>
2021-06-22 20:39:34 -0300

    Tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import "reflect-metadata";
 import { router } from "./routes";
 import "./database";
@@ -7,5 +8,19 @@ import "./database";
 const app = express();
 app.use(express.json());
 app.use(router);
+
+// Tratando erros
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+})
 
 app.listen(3000, () => console.log('Server is running at port 3000!'));
