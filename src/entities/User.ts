eb44328850c0a35f5a7f96e4c24b73b03a8b62f4
Tungsinn <gvnalamas@tutanota.com>
2021-06-25 02:33:50 -0300

    Excluindo a exibição de alguns atributos do usuário da listagem

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,10 +1,12 @@
 import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import { Exclude } from "class-transformer";
 // a v4 gera números totalmente aleatórios
 import { v4 as uuid } from "uuid";
 
 @Entity("users")
 class User {
 
+    @Exclude()
     @PrimaryColumn()
     // A alteração desse valor só é feita pela própria entidade com a biblioteca uuid
     readonly id: string;
@@ -12,18 +14,22 @@ class User {
     @Column()
     name: string;
 
+    @Exclude()
     @Column()
     password: string;
 
     @Column()
     email: string;
 
+    @Exclude()
     @Column()
     admin: boolean;
 
+    @Exclude()
     @CreateDateColumn()
     created_at: Date;
 
+    @Exclude()
     @UpdateDateColumn()
     updated_at: Date;
 
