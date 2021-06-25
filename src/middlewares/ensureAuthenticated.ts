960dac6040208065957811d04437c8080775d7b2
Tungsinn <gvnalamas@tutanota.com>
2021-06-25 01:31:31 -0300

    Autenticando usuário e admin com JWT

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+    sub: string;
+}
+
+export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+    
+    // Receber o token
+    const authToken = request.headers.authorization;
+    
+    // Validar se token está preenchido
+    if(!authToken) {
+        return response.status(401).end();
+    }
+
+    // A primeira posição do array é ignorada e a segunda posição é armazenada na variável token
+    const [,token] = authToken.split(" ");
+
+    try {
+        // Validar se token não está expirado | usuário autenticado
+        const { sub } = verify(token, "7528a6b0833feb683a29d0a845f8225f") as IPayload;
+        
+        request.user_id = sub;
+
+        return next();
+    } catch(err) {
+        return response.status(401).end();
+    }
+    
+    // Recuperar informações do usuário
+    
+}
